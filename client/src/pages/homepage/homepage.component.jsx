936f47ce0fb68a98db6d625101dfa67420043b41
amit-lekhak <amitlekhak1@gmail.com>
2021-03-22 19:31:56 +0545

    Add Test files and snapshots with jest and enzyme

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,6 @@ import Directory from '../../components/directory/directory.component';
 import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => (
-  
   <HomePageContainer>
     <Directory />
   </HomePageContainer>
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -4,21 +4,26 @@ import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
+import Spinner from '../../components/spinner/spinner.component';
+
+import { ShopPageContainer } from './shop.styles';
+
 const CollectionsOverviewContainer = lazy(() =>
   import('../../components/collections-overview/collections-overview.container')
 );
+
 const CollectionPageContainer = lazy(() =>
   import('../collection/collection.container')
 );
 
-const ShopPage = ({ fetchCollectionsStart, match }) => {
+export const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
     fetchCollectionsStart();
   }, [fetchCollectionsStart]);
 
   return (
-    <div className='shop-page'>
-      <Suspense>
+    <ShopPageContainer>
+      <Suspense fallback={<Spinner />}>
         <Route
           exact
           path={`${match.path}`}
@@ -29,12 +34,15 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
           component={CollectionPageContainer}
         />
       </Suspense>
-    </div>
+    </ShopPageContainer>
   );
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
